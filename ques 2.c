48aa1d6c5f3f12c1db54c1bd061ba0289564001d
lalitsharma728 <luckykaushik728@gmail.com>
2026-06-18 22:05:11 +0530

    Read an integer /float/char and print it.

---
+++ ques 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i,float f,char c;
+    printf("enter an integer,a float,and a character");
+    scanf("%d %f %c",&i,&f,&c);
+    printf("\ninteger= %d",i);
+    printf("\nfloat= %f",f);
+    printf("\ncharacter= %c",c);
+    return 0;
+}

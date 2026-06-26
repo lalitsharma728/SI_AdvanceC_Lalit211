dd98582e071601ebdc7363c25b01eaed2d540d8f
lalitsharma728 <luckykaushik728@gmail.com>
2026-06-26 13:54:06 +0530

    Add files via upload

---
+++ Assignment 1 =rock scissor paper.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int user, comp;
+    int userScore = 0, compScore = 0, draws = 0;
+
+    srand(time(NULL));
+
+    while (1) {
+        printf("\n===== ROCK PAPER SCISSORS =====\n");
+        printf("1. Rock\n");
+        printf("2. Paper\n");
+        printf("3. Scissors\n");
+        printf("0. Exit\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &user);
+
+        if (user == 0) {
+            printf("\n===== FINAL SCORE =====\n");
+            printf("You: %d\n", userScore);
+            printf("Computer: %d\n", compScore);
+            printf("Draws: %d\n", draws);
+
+            if (userScore > compScore)
+                printf("Overall Winner: You!\n");
+            else if (compScore > userScore)
+                printf("Overall Winner: Computer!\n");
+            else
+                printf("Overall Result: Tie!\n");
+
+            printf("\nThanks for playing!\n");
+            break;
+        }
+
+        if (user < 1 || user > 3) {
+            printf("Invalid choice! Try again.\n");
+            continue;
+        }
+
+        comp = rand() % 3 + 1;
+
+        printf("\nYou chose: ");
+        if (user == 1)
+            printf("Rock");
+        else if (user == 2)
+            printf("Paper");
+        else
+            printf("Scissors");
+
+        printf("\nComputer chose: ");
+        if (comp == 1)
+            printf("Rock");
+        else if (comp == 2)
+            printf("Paper");
+        else
+            printf("Scissors");
+
+        printf("\n");
+
+        if (user == comp) {
+            printf("Result: Draw!\n");
+            draws++;
+        }
+        else if ((user == 1 && comp == 3) ||
+                 (user == 2 && comp == 1) ||
+                 (user == 3 && comp == 2)) {
+            printf("Result: You Win!\n");
+            userScore++;
+        }
+        else {
+            printf("Result: Computer Wins!\n");
+            compScore++;
+        }
+
+        printf("\nCurrent Score\n");
+        printf("You: %d | Computer: %d | Draws: %d\n",
+               userScore, compScore, draws);
+    }
+
+    return 0;
+}
